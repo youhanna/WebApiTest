da544ed2f7725d803b9ecc5342e1e6e06cba4b9d
Youhanna Mesiha <youhanna.mesiha@dh.com>
2017-11-09 12:42:18 -0500

    Add project files.

---
+++ RestApi/Controllers/ValuesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using RestApi.Models;
+
+namespace RestApi.Controllers
+{
+    //[Authorize]
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        public IHttpActionResult Get(string input)
+        {
+            //return Ok(new string[] { "value1", "value2" });
+            return Ok(new ValuesModel {ValueString = input});
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public IHttpActionResult Post(ValuesModel input)
+        {
+            return Ok(new ValuesModel {ValueString = "PostValue: "+input.ValueString});
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ RestApi/Models/ValuesModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestApi.Models
+{
+    public class ValuesModel
+    {
+        public string ValueString;
+    }
+}
